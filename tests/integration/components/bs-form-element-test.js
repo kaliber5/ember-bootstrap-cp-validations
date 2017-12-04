97c176dc6ff81fe939250301337d275e95e05b86
David Baker <dbaker@acorncomputersolutions.com>
2017-12-04 14:52:43 -0700

    Ensure that buffered proxy objects don't fail unexpectedly when validating

--- tests/integration/components/bs-form-element-test.js
+++ tests/integration/components/bs-form-element-test.js
@@ -1,4 +1,5 @@
 import EmberObject from '@ember/object';
+import ObjectProxy from '@ember/object/proxy';
 import { setOwner } from '@ember/application';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
@@ -12,6 +13,9 @@ const Validations = buildValidations({
   ]
 });
 const Model = EmberObject.extend(Validations, {
+  test: null
+});
+const BufferedModel = ObjectProxy.extend(Validations, {
   test: null
 });
 
@@ -65,5 +69,62 @@ module('Integration | Component | bs form element', function(hooks) {
 
     await triggerEvent('form', 'submit');
     assert.ok(find('.form-group').classList.contains('has-error'), 'form element group has error class');
+  });
+
+  test('valid validation is supported as expected when working with an ember-buffered-proxy model', async function(assert) {
+    let proxiedModel = Model.create();
+    setOwner(proxiedModel, this.owner);
+
+    let model = BufferedModel.create({
+      content: proxiedModel,
+      test: '123'
+    });
+    setOwner(model, this.owner);
+
+    this.set('model', model);
+    this.submitAction = function() {
+      assert.ok(true, 'submit action has been called.');
+    };
+    this.invalidAction = function() {
+      assert.ok(false, 'Invalid action must not been called.');
+    };
+
+    await render(hbs`
+    {{#bs-form model=model onSubmit=(action submitAction) onInvalid=(action invalidAction) as |form|}}
+      {{form.element label="test" property="test"}}
+    {{/bs-form}}
+  `);
+
+    assert.expect(1);
+
+    await triggerEvent('form', 'submit');
+  });
+
+  test('invalid validation is supported as expected when working with an ember-buffered-proxy model', async function(assert) {
+    let proxiedModel = Model.create();
+    setOwner(proxiedModel, this.owner);
+
+    let model = BufferedModel.create({
+      content: proxiedModel
+    });
+    setOwner(model, this.owner);
+
+    this.set('model', model);
+    this.submitAction = function() {
+      assert.ok(false, 'submit action must not been called.');
+    };
+    this.invalidAction = function() {
+      assert.ok(true, 'Invalid action has been called.');
+    };
+
+    await render(hbs`
+    {{#bs-form model=model onSubmit=(action submitAction) onInvalid=(action invalidAction) as |form|}}
+      {{form.element label="test" property="test"}}
+    {{/bs-form}}
+  `);
+
+    assert.expect(1);
+
+    await triggerEvent('form', 'submit');
   });
 });
